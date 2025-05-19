abeca49c17593df23d0ce82772a69b188a892f37
Smit Patel <113251493+smit0308@users.noreply.github.com>
2025-05-19 20:43:14 +0530

    Update authFeature.js

--- src/redux/services/authFeature.js
+++ src/redux/services/authFeature.js
@@ -27,8 +27,8 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use((response) => response, (error) => {
     if (error.response?.status === 401) {
         // Handle unauthorized access
-        localStorage.removeItem("user");
-        window.location.href = "/login";
+        
+        window.location.href = "/";
     } else if (error.response?.status === 403) {
         // Handle email verification needed
         localStorage.removeItem("user");
